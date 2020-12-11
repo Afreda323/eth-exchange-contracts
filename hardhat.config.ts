bd9bd8804617ba7a4a79a81b5495147851b13fac
Anthony Freda <anthonyfreda323@gmail.com>
2020-12-11 13:27:42 -0500

    cleanup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,22 +7,31 @@ import 'hardhat-typechain'
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || ''
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY!
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
-const config = {
+const rinkeby =
+  INFURA_API_KEY && RINKEBY_PRIVATE_KEY
+    ? {
+        url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+        accounts: [RINKEBY_PRIVATE_KEY],
+      }
+    : {}
+
+const etherscan = ETHERSCAN_API_KEY
+  ? {
+      apiKey: ETHERSCAN_API_KEY,
+    }
+  : undefined
+
+const config: HardhatUserConfig = {
   solidity: '0.7.3',
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {},
     localhost: {},
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [RINKEBY_PRIVATE_KEY],
-    },
+    rinkeby,
   },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  },
+  etherscan,
 }
 
 export default config
