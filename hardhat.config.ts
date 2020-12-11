284eb575842afd0afe23f887091c4c87077bc805
Anthony Freda <anthonyfreda323@gmail.com>
2020-12-11 12:31:44 -0500

    moar setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,28 @@
 import { task, HardhatUserConfig } from "hardhat/config";
+import dotEnvExtended from "dotenv-extended";
+dotEnvExtended.load();
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-typechain";
+import "@nomiclabs/hardhat-etherscan";
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY!;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
 
 const config: HardhatUserConfig = {
   solidity: "0.7.3",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    localhost: {},
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
