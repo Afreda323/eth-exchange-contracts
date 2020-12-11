d95e13e219322e1bd048ff67d6f2b39ab2e0f42f
Anthony Freda <anthonyfreda323@gmail.com>
2020-12-11 12:11:08 -0500

    typesafe

--- test/Greeter.test.ts
+++ test/Greeter.test.ts
@@ -1,10 +1,11 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
+import { Greeter } from "../typechain/Greeter";
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+    const greeterFactory = await ethers.getContractFactory("Greeter");
+    const greeter = (await greeterFactory.deploy("Hello, world!")) as Greeter;
 
     await greeter.deployed();
     expect(await greeter.greet()).to.equal("Hello, world!");
