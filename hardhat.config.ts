064bb89005f558cc199a7b2b0c7e40b4cb96738b
Anthony Freda <anthonyfreda323@gmail.com>
2020-12-11 13:24:01 -0500

    husky

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,17 @@
-import { task, HardhatUserConfig } from "hardhat/config";
-import dotEnvExtended from "dotenv-extended";
-dotEnvExtended.load();
-import "@nomiclabs/hardhat-waffle";
-import "hardhat-typechain";
-import "@nomiclabs/hardhat-etherscan";
+import { HardhatUserConfig } from 'hardhat/types/config'
+import dotEnvExtended from 'dotenv-extended'
+dotEnvExtended.load()
+import '@nomiclabs/hardhat-waffle'
+import '@nomiclabs/hardhat-etherscan'
+import 'hardhat-typechain'
 
-const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY!;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
+const INFURA_API_KEY = process.env.INFURA_API_KEY || ''
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY!
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!
 
-const config: HardhatUserConfig = {
-  solidity: "0.7.3",
-  defaultNetwork: "hardhat",
+const config = {
+  solidity: '0.7.3',
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {},
     localhost: {},
@@ -23,6 +23,6 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
-};
+}
 
-export default config;
+export default config
